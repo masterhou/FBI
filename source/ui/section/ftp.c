8f4ae1c1f5089dc5ab47909328324afc98d35399
lava3d <ythou@Thomas-MBP.local>
2016-04-29 11:26:49 +0800

    ftp in back thread

--- source/ui/section/ftp.c
+++ source/ui/section/ftp.c
@@ -14,10 +14,11 @@
 #include "../prompt.h"
 #include "../../core/screen.h"
 #include "../../ftpd/ftp.h"
-#include "../../ftpd/console.h"
 
 typedef struct{
-    bool ftpInitialised;
+    bool initialised;
+    bool finished;
+    Handle cancelEvent;
 }ftp_data;
 
 static void ftp_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2) {
@@ -40,44 +41,70 @@ static void ftp_draw_top(ui_view* view, void* data, float x1, float y1, float x2
     float verY = logoY + logoHeight + (y2 - (logoY + logoHeight) - verHeight) / 2;
     screen_draw_string(verString, verX, verY, 0.5f, 0.5f, COLOR_TEXT, false);
 }
-      
+
+static void ftp_thread(void* arg) {
+    ftp_data* data = (ftp_data*) arg;
+    sdmcWriteSafe(false);
+    while(svcWaitSynchronization(data->cancelEvent, 0) != 0){
+        /* done with ftp */
+        if(ftp_loop() == LOOP_EXIT) 
+            break;
+    }
+    ftp_exit();
+    sdmcWriteSafe(true);
+    data->finished = true;
+}      
+
 static void ftp_wait_update(ui_view* view, void* data, float* progress, char* text) {
     ftp_data*  ftpdata= (ftp_data*)data;
 
-    if(hidKeysDown() & KEY_B) {
-        sdmcWriteSafe(true);
-        if(ftpdata->ftpInitialised)
-            ftp_exit();
+    if(ftpdata->finished){
+        svcCloseHandle(ftpdata->cancelEvent);
+        free(ftpdata);
+
         ui_pop();
         info_destroy(view);
-        free(ftpdata);
-        return;
+
+        return;       
     }
-    if(ftpdata->ftpInitialised) {
+
+    if(hidKeysDown() & KEY_B) {
+        svcSignalEvent(ftpdata->cancelEvent);
+    }
+
+    if(ftpdata->initialised) {
         struct in_addr addr = {(in_addr_t) gethostid()};
         snprintf(text, PROGRESS_TEXT_MAX, "Ready!\nIP: %s\nPort: 5000", inet_ntoa(addr));
-        /* ftp loop */
-        if(ftp_loop() == LOOP_EXIT) {
-            /* done with ftp */
-            ftp_exit();
-            ftpdata->ftpInitialised = false;
-        }
     }
     else {
         u32    wifi = 0;
         Result ret = ACU_GetWifiStatus(&wifi);
         if(ret == 0)
-            ftpdata->ftpInitialised = (ftp_init() == 0);
-
+            ftpdata->initialised = (ftp_init() == 0);
+        if(ftpdata->initialised){
+            if(threadCreate(ftp_thread, data, 0x10000, 0x18, 1, true) == NULL) {
+                //error_display(NULL, NULL, NULL, "Failed to create ftp loop thread.");
+                ftpdata->finished = true; 
+                return;
+            }
+        }
         snprintf(text, PROGRESS_TEXT_MAX, "Waiting for wifi...\n");
     }
 }
 
 void ftp_open() {
-    sdmcWriteSafe(false);
     /* initialize ftp subsystem */
     ftp_data* data = (ftp_data*) calloc(1, sizeof(ftp_data));
-    data->ftpInitialised = false;
+    data->initialised = false;
+    data->finished = false;
+
+    Result eventRes = svcCreateEvent(&data->cancelEvent, 1);
+    if(R_FAILED(eventRes)) {
+        error_display_res(NULL, NULL, NULL, eventRes, "Failed to create ftp loop cancel event.");
+        free(data);
+        return;
+    }
+
     info_display("FTP", "B: Return", false, data, ftp_wait_update, ftp_draw_top);
 }
 
